a7ff50233ae5eac980496f3790d8e22478b9e27c
Anton Kashirov <anton.kashirov@gmail.com>
2026-06-13 15:08:55 +0300

    PR1: SSR skeleton + quality-gate & release infrastructure (#1)

---
+++ src/shared/lib/index.ts
@@ -0,0 +1,4 @@
+export { container, type IDisposable } from './DIContainer'
+export { useViewModel } from './hooks/useViewModel'
+export { useService } from './hooks/useService'
+export { useHydrated } from './hooks/useHydrated'
